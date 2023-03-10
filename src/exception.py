1ff093ec91b5ec3e90d72a82a1c09cdd345c2159
vinit <vinit.londhe21@gmail.com>
2023-03-11 01:42:02 +0530

    EDA and model training

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-import logging
+from src.logger import logging
 
 def error_message_details(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
@@ -15,4 +15,3 @@ class CustomException(Exception):
 
     def __str__(self):
         return self.error_message
-    
